642f3e0672ecd57028fe44cbba491d45eb203146
marufhasan <marufhasan6174@gmail.com>
2025-07-14 11:40:39 +0600

    project initialization ...

---
+++ backend/src/main/java/com/medilens/app/MedilensApplication.java
@@ -0,0 +1,13 @@
+package com.medilens.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilensApplication.class, args);
+	}
+
+}

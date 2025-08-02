6e7e74665b3bcdebe01c52e2897799744c89d2c9
Maruf Hasan <marufhasan6174@gmail.com>
2025-08-02 12:12:58 +0600

    Working on admin panel ...

--- backend/src/main/java/com/medilens/app/model/Role.java
+++ backend/src/main/java/com/medilens/app/model/Role.java
@@ -3,5 +3,4 @@ package com.medilens.app.model;
 public enum Role {
     ROLE_USER,
     ROLE_ADMIN,
-    ROLE_DOCTOR
 }
--- backend/src/main/java/com/medilens/app/service/UserService.java
+++ backend/src/main/java/com/medilens/app/service/UserService.java
@@ -5,19 +5,24 @@ import com.medilens.app.DTO.UserDTO;
 import com.medilens.app.exception.NotFoundException;
 import com.medilens.app.model.Chat;
 import com.medilens.app.model.User;
+import org.apache.coyote.BadRequestException;
 
 import java.util.List;
 
 public interface UserService {
-    UserDTO getUserByEmail(String email) throws NotFoundException;
+    UserDTO getUserByEmail(String email) throws NotFoundException, BadRequestException;
+    UserDTO getAdminByEmail(String email) throws NotFoundException, BadRequestException;
 
     UserDTO save(User user) ;
 
-    List<UserDTO> findAll();
+    List<UserDTO> findAllUsers();
+    List<UserDTO> findAllAdmin();
 
     void delete(String email) throws NotFoundException;
 
     UserDTO update(User user);
 
     List<ChatDTO> getAllChats(String email) throws NotFoundException;
+
+
 }
